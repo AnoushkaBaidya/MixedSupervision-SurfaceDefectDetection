c611539ced2b5dddceaace286c65887d3a9070ce
Anoushka Baidya <115124698+AnoushkaBaidya@users.noreply.github.com>
2025-02-11 14:40:53 -0600

    Added Inference File

--- Inference.py
+++ Inference.py
@@ -12,15 +12,15 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
 # Configuration
-#MODEL_PATH = "/home/ubuntu/Projects/MixedSupervision-SurfaceDefectDetection/RESULTS/TRAININGDATASET/experiment_1/models/final_state_dict.pth"  # Path to the saved model
-#TEST_IMAGES_DIR = "/home/ubuntu/Projects/TestImages/8_Nov_Test_Dataset"  # Directory containing test images
-#OUTPUT_DIR = "/home/ubuntu/Projects/MixedSupervision-SurfaceDefectDetection/SavePredictions"  # Directory to save predictions
-#EVAL_OUTPUT_DIR = "/home/ubuntu/Projects/MixedSupervision-SurfaceDefectDetection/EvaluationResults"  # Directory to save evaluation results
+MODEL_PATH = "/home/ubuntu/Projects/MixedSupervision-SurfaceDefectDetection/RESULTS/TRAININGDATASET/experiment_1/models/final_state_dict.pth"  # Path to the saved model
+TEST_IMAGES_DIR = "/home/ubuntu/Projects/TestImages/8_Nov_Test_Dataset"  # Directory containing test images
+OUTPUT_DIR = "/home/ubuntu/Projects/MixedSupervision-SurfaceDefectDetection/SavePredictions"  # Directory to save predictions
+EVAL_OUTPUT_DIR = "/home/ubuntu/Projects/MixedSupervision-SurfaceDefectDetection/EvaluationResults"  # Directory to save evaluation results
 
-MODEL_PATH = "/Users/anoushka/Desktop/Projects/RESULTS/TRAININGDATASET/experiment_1/models/best_state_dict.pth"  # Path to the saved model
-TEST_IMAGES_DIR = "/Users/anoushka/Desktop/Projects/MixedSupervision-SurfaceDefectDetection/TestImages/8_Nov_Test_Dataset"  # Directory containing test images
-OUTPUT_DIR = "/Users/anoushka/Desktop/Projects/MixedSupervision-SurfaceDefectDetection//SavePredictions"  # Directory to save predictions
-EVAL_OUTPUT_DIR = "/Users/anoushka/Desktop/Projects/MixedSupervision-SurfaceDefectDetection/EvaluationResults"  # Directory to save evaluation results
+#MODEL_PATH = "/Users/anoushka/Desktop/Projects/RESULTS/TRAININGDATASET/experiment_1/models/best_state_dict.pth"  # Path to the saved model
+#TEST_IMAGES_DIR = "/Users/anoushka/Desktop/Projects/MixedSupervision-SurfaceDefectDetection/TestImages/8_Nov_Test_Dataset"  # Directory containing test images
+#OUTPUT_DIR = "/Users/anoushka/Desktop/Projects/MixedSupervision-SurfaceDefectDetection//SavePredictions"  # Directory to save predictions
+#EVAL_OUTPUT_DIR = "/Users/anoushka/Desktop/Projects/MixedSupervision-SurfaceDefectDetection/EvaluationResults"  # Directory to save evaluation results
 
 INPUT_WIDTH = 256  # Input image width for model processing
 INPUT_HEIGHT = 256  # Input image height for model processing
